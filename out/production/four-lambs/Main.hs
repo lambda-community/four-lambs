5f0c23fc6438cf2e3ae679ecf13b1c876c5abb72
diego murillo <diego.spd@gmail.com>
2020-06-22 17:40:41 -0500

    git ignore out/

--- out/production/four-lambs/Main.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import FourLambs
-
-main :: IO ()
-main = someFunc
--- out/production/four-lambs/MaxHeap.hs
+++
@@ -1,7 +0,0 @@
-module MaxHeap where
-
-data Tree a = Node (Tree a) (Tree a)
-            | Leaf a
-            deriving (Eq, Show, Ord)
-
-

edd289bc6d28861a868e1803507874f1c5d2733a
diego murillo <diego.albarran@nubank.com.br>
2020-06-23 18:57:05 -0500

    homework: replace undefined with correct expressions to implement sum'' and mult

--- src/FourLambs.hs
+++ src/FourLambs.hs
@@ -13,14 +13,14 @@ take' n (x:xs)
   | n >  0    = x:(take' (n-1) xs)
   | otherwise = reverse' (take' (negate n) (reverse' (x:xs)))
 
--- | With hand written recursion
+-- with hand written recursion
 any' :: (a -> Bool) -> [a] -> Bool
 any' f []     = False
 any' f (x:xs)
   | f x       = True
   | otherwise = any' f xs
 
--- | Implicit recursion via reduce
+-- implicit recursion via reduce
 any'' :: (a -> Bool) -> [a] -> Bool
 any'' f xs = reduce' (\x y -> f x || y) False xs
 
@@ -28,9 +28,15 @@ sum' :: Num a => [a] -> a
 sum' []     = 0
 sum' (x:xs) = x + (sum' xs)
 
-mult' :: Num a => [a] -> a8
+sum'' :: Num a => [a] -> a
+sum'' xs = reduce' undefined undefined xs
+
+mult' :: Num a => [a] -> a
 mult' []     = 1
 mult' (x:xs) = x * (mult' xs)
+
+mult'' :: Num a => [a] -> a
+mult'' xs = reduce' undefined undefined xs
 
 reduce' :: (a -> b -> b) -> b -> [a] -> b
 reduce' f z []     = z

c6fec2e655cb1be72d2dc3749bf5a8f0208a46a4
diego murillo <diego.spd@gmail.com>
2020-07-01 14:33:10 -0500

    drop

--- src/Fractals/Types.hs
+++
@@ -1,9 +0,0 @@
-module Fractals.Types where
-import Prelude hiding (Real)
-
-
--- | Complex numbers, the real part is on the left
--- the imaginary part is on the right
-type Real = Double
-type Imaginary = Double
-type Complex = (Real, Imaginary)
